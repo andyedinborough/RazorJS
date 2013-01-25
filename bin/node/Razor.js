f1c36f6742ea7c645f221af0a399488b897c2cde
Andy Edinborough <andy@edinborough.org>
2013-01-25 15:12:45 -0600

    node fix

--- bin/node/Razor.js
+++ bin/node/Razor.js
@@ -446,4 +446,39 @@ var Razor = {
 	basePage: basePage, Cmd: Cmd, extend: extend,
 	render: function (markup, model, page) { return compile(markup)(model, page); }
 };
+Razor.findView = function findViewInFileSystem(viewName, cb) {
+  var fs = require('fs');
+  if (!viewName.match(/\w+\.\w+$/i))
+    viewName += '.jshtml';
+  viewName = './' + viewName;
+
+  var done = function (err, data) {
+    if (err) {
+      console.error("Could not open file: %s", err);
+      process.exit(1);
+    }
+
+    if(cb) cb(data.toString('utf-8'));
+  };
+
+  if(cb) return void fs.readFile(viewName, done);
+  fs.readFileSync(viewName, done);
+};
+
+var wrapper;
+Razor.precompile = function(code, page) {
+  if(!page) page = {}; 
+  code = 'var page1 = ' + JSON.stringify(page) + 
+    ', func = function(){ ' + Razor.parse(code) + ' }';
+  if(!wrapper) wrapper = Razor.compile('');
+
+  code = '(function(){ ' + code + ';\nreturn ' + wrapper + '; })()';
+  code = code
+    .replace(/(\W)extend(\W)/g, '$1Razor.extend$2')
+    .replace(/(\W)basePage(\W)/g, '$1Razor.basePage$2');
+
+  return code;
+};
+
+module.Razor = module.exports = Razor; 
 })(global, module);
